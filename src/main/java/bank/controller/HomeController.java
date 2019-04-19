0c662c6b81a5fb28f29fc5c84c997e825762935c
Dan6667 <33529082+Dan6667@users.noreply.github.com>
2019-04-19 06:50:13 +0300

    new controllers provided & configuration

--- src/main/java/bank/controller/HomeController.java
+++ src/main/java/bank/controller/HomeController.java
@@ -3,13 +3,14 @@ package bank.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = GET)
     public String home(Model model){
-        return "index";
+        return "home";
     }
 }
--- src/main/java/bank/controller/RegisterController.java
+++ src/main/java/bank/controller/RegisterController.java
@@ -22,6 +22,6 @@ public class RegisterController {
     public String registerProcessing(Client client){
         new ClientDao().saveClient(client);
         bank.addClient(client);
-        return "index";
+        return "home";
     }
 }
