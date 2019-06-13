4249e77baeef1885cf937d8bff18ea82ea2618b8
Dan6667 <33529082+Dan6667@users.noreply.github.com>
2019-06-13 03:35:37 +0300

    swagger fixed & mapping slightly changed

--- src/main/java/bank/config/BankConfiguration.java
+++ src/main/java/bank/config/BankConfiguration.java
@@ -1,19 +1,18 @@
 package bank.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 @Configuration
 @ComponentScan(basePackages = {"bank"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = {"bank.controller.*", "bank.config.*"})})
 @EnableAspectJAutoProxy
 @EnableScheduling
 public class BankConfiguration {
-//
-//    @Bean
-//    public TaskScheduler taskScheduler() {
-//        return new ConcurrentTaskScheduler();
-//    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
 }
